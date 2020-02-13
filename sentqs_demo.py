12dea649e309ffa8fd809caccfc9724db5148d7e
Christoph <christophraab@outlook.de>
2020-02-13 15:52:57 +0100

    New Demo File!

--- sentqs_demo.py
+++ sentqs_demo.py
@@ -7,6 +7,13 @@ authors:  Christoph Raab
 """
 
 
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D,Conv2D
+from keras.datasets import imdb
 import requests
 import sys
 import numpy as np
@@ -42,6 +49,7 @@ def download_data():
     return Xs,Ys,Xt,Yt
 
 # Xs,Ys,Xt,Yt = download_data()
+
 #If dataset file is already downloaded
 data = np.load(file_name,allow_pickle=True)
 Xs = data["arr_0"]
@@ -49,14 +57,42 @@ Ys = data["arr_1"]
 Xt = data["arr_2"]
 Yt = data["arr_3"]
 
-# print("\n Classification Task Test \n")
-# clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0).fit(Xs, Ys)
-# print(clf.score(Xt, Yt))
+# Convolution
+kernel_size = 5
+filters = 64
+pool_size = 4
 
-nbt = NBT(landmarks=60000)
-# Data augmentation is necessary if Z and X have different shapes.
-Ys, Xs = nbt.data_augmentation(Xs, Xt.shape[0], Ys)
-X, Xs = nbt.nys_basis_transfer(Xt, Xs, Ys.flatten())
-clf = GradientBoostingClassifier()
-clf.fit(Xs, Ys)
-print(clf.score(Xt, Yt))
+# Training
+batch_size = 256
+epochs = 2
+# LSTM
+
+lstm_output_size = 70
+Xs = np.expand_dims(Xs, 2)
+Xt = np.expand_dims(Xt, 2)
+
+
+model = Sequential()
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(35))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+# model.summary()
+print('Train...')
+model.fit(Xs, Ys,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(Xt, Yt))
+score, acc = model.evaluate(Xt, Xt, batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
