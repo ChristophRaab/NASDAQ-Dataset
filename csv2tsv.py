48c5a0d01b8fb86fbcb934fbffb9f483dc075cd9
Christian Niekler <christian.niekler@gmail.com>
2021-09-14 21:40:35 -0400

    add csv2tsv

---
+++ csv2tsv.py
@@ -0,0 +1,12 @@
+import csv
+from pathlib import Path
+
+csv_file = Path(__file__).with_name('Tweets.csv')
+tsv_file = Path(__file__).with_name('Tweets.tsv')
+
+with csv_file.open('r', encoding='utf8') as csvin, tsv_file.open('w', encoding='utf8') as tsvout:
+    csvin = csv.reader(csvin)
+    tsvout = csv.writer(tsvout, delimiter='\t')
+
+    for row in csvin:
+        tsvout.writerow(row)
