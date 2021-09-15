fdec1b87321157c98bac08e2e22a7761d52b8199
Christian Niekler <christian.niekler@gmail.com>
2021-09-14 21:45:44 -0400

    add some comments to csv2tsv

--- csv2tsv.py
+++ csv2tsv.py
@@ -1,9 +1,11 @@
 import csv
 from pathlib import Path
 
+# this reads the files that are in the same directory
 csv_file = Path(__file__).with_name('Tweets.csv')
 tsv_file = Path(__file__).with_name('Tweets.tsv')
 
+# read every line from the comma separated file and save it in a tab separted file
 with csv_file.open('r', encoding='utf8') as csvin, tsv_file.open('w', encoding='utf8') as tsvout:
     csvin = csv.reader(csvin)
     tsvout = csv.writer(tsvout, delimiter='\t')
